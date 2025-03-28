6c9bcc203133748bb632610d1ea3149edfa88ce2
rjenkins <ray@streamfold.com>
2025-03-28 16:47:02 -0700

    remove unhelpful comment

--- src/receivers/mod.rs
+++ src/receivers/mod.rs
@@ -7,7 +7,6 @@ pub mod otlp_output;
 use opentelemetry::global;
 use opentelemetry::metrics::Meter;
 
-// Create a meter from the above MeterProvider.
 pub fn get_meter() -> Meter {
     global::meter("receivers")
 }
