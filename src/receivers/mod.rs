a1a14c310c5bfd26654205c274e59bbbf2a54151
rjenkins <ray@streamfold.com>
2025-03-28 16:10:22 -0700

    add scope get_meter package function for receivers

--- src/receivers/mod.rs
+++ src/receivers/mod.rs
@@ -3,3 +3,11 @@
 pub mod otlp_grpc;
 pub mod otlp_http;
 pub mod otlp_output;
+
+use opentelemetry::global;
+use opentelemetry::metrics::Meter;
+
+// Create a meter from the above MeterProvider.
+pub fn get_meter() -> Meter {
+    global::meter("receivers")
+}
