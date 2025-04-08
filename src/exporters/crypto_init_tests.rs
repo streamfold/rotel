2ccda82fbec3c0300e77cafb85e14ab67a31ee6d
rjenkins <ray@streamfold.com>
2025-04-08 09:28:50 -0700

    remove comment

--- src/exporters/crypto_init_tests.rs
+++ src/exporters/crypto_init_tests.rs
@@ -4,7 +4,6 @@ use std::sync::Once;
 #[cfg(test)]
 static INIT_CRYPTO: Once = Once::new();
 
-// Allowing dead_code because this is for unit tests ONLY!
 #[cfg(test)]
 pub fn init_crypto() {
     INIT_CRYPTO.call_once(|| {
