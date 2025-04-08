2c5fdfc4856a3db8b6d4b0ef637252cad929cbc3
rjenkins <ray@streamfold.com>
2025-04-08 09:28:15 -0700

    add cfg test

--- src/exporters/crypto_init_tests.rs
+++ src/exporters/crypto_init_tests.rs
@@ -1,9 +1,11 @@
+#[cfg(test)]
 use std::sync::Once;
 
+#[cfg(test)]
 static INIT_CRYPTO: Once = Once::new();
 
 // Allowing dead_code because this is for unit tests ONLY!
-#[allow(dead_code)]
+#[cfg(test)]
 pub fn init_crypto() {
     INIT_CRYPTO.call_once(|| {
         rustls::crypto::ring::default_provider()
