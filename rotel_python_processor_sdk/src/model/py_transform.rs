b3cf23c2755519b52c2fbf40aaacb53bfc7d60b8
Mike Heffner <mikeh@fesnel.com>
2025-09-04 17:12:35 -0400

    cargo fmt

--- rotel_python_processor_sdk/src/model/py_transform.rs
+++ rotel_python_processor_sdk/src/model/py_transform.rs
@@ -735,13 +735,28 @@ pub fn transform_resource(
     let entity_refs = Arc::into_inner(resource.entity_refs).unwrap();
     let entity_refs = entity_refs.into_inner().unwrap();
     for entity_ref_arc in entity_refs.into_iter() {
-        let moved_data = Arc::into_inner(entity_ref_arc).unwrap().into_inner().unwrap();
+        let moved_data = Arc::into_inner(entity_ref_arc)
+            .unwrap()
+            .into_inner()
+            .unwrap();
 
         new_entity_refs.push(opentelemetry_proto::tonic::common::v1::EntityRef {
-            schema_url: Arc::into_inner(moved_data.schema_url).unwrap().into_inner().unwrap(),
-            r#type: Arc::into_inner(moved_data.r#type).unwrap().into_inner().unwrap(),
-            id_keys: Arc::into_inner(moved_data.id_keys).unwrap().into_inner().unwrap(),
-            description_keys: Arc::into_inner(moved_data.description_keys).unwrap().into_inner().unwrap(),
+            schema_url: Arc::into_inner(moved_data.schema_url)
+                .unwrap()
+                .into_inner()
+                .unwrap(),
+            r#type: Arc::into_inner(moved_data.r#type)
+                .unwrap()
+                .into_inner()
+                .unwrap(),
+            id_keys: Arc::into_inner(moved_data.id_keys)
+                .unwrap()
+                .into_inner()
+                .unwrap(),
+            description_keys: Arc::into_inner(moved_data.description_keys)
+                .unwrap()
+                .into_inner()
+                .unwrap(),
         });
     }
 
